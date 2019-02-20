17f0563a8479ac93a1355389d7f63fd21f9b69f4
Kenn <kenn+git@code-crew.org>
2019-02-20 11:06:26 -0600

    update

---
+++ product_movie/product_movieApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProductMovieappConfig(AppConfig):
+    name = 'product_movieApp'
---
+++ product_movie/product_movieApp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(decimal_places=3, max_digits=9)
+    quantity = models.IntegerField()
