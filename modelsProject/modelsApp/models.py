38b8e14d2de49c67d67a8df3ed810eff7e2fb48b
Kenn <kenn+git@code-crew.org>
2019-02-20 10:01:07 -0600

    upload

--- modelsProject/modelsApp/models.py
+++ modelsProject/modelsApp/models.py
@@ -13,3 +13,9 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+# name, stars, and releaseDate
+class Movie(models.Model):
+    name = models.CharField(max_length=150)
+    starRating = models.DecimalField(decimal_places=1, max_digits=3)
+    releaseDate = models.DateField()
