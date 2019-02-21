87516171213e06d7bdf95a3977748d5a19cc3fe4
Kenn <kenn+git@code-crew.org>
2019-02-21 09:49:52 -0600

    update

--- product_movie/product_movieApp/models.py
+++ product_movie/product_movieApp/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(decimal_places=3, max_digits=9)
     quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- product_movie/product_movieApp/views.py
+++ product_movie/product_movieApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def index(request):
+    databaseArray = Product.objects.all()
+    print(databaseArray)
     return HttpResponse("Test A new URL")
