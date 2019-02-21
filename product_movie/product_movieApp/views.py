c9543f7d64ef2d7ea23941025d91d6ea95c76805
Kenn <kenn+git@code-crew.org>
2019-02-21 10:17:28 -0600

    update

--- product_movie/product_movieApp/views.py
+++ product_movie/product_movieApp/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
-    databaseArray = Product.objects.all()
-    print(databaseArray)
-    return HttpResponse("Test A new URL")
+    databaseArray = Product.objects.filter(quantity__lt = 2)
+    print(databaseArray[0].quantity)
+    databaseArray[0].quantity = 500
+    databaseArray[0].save()
+    return HttpResponse(databaseArray)
+
+    # newObject = Product(name="Something Else", price=100, quantity = 5)
+    # # newObject = Movie(name="something", starrating = 10, releaseDate = "")
+    # newObject.save()
+    #
+    # databaseArray = Product.objects.all()
+    # # print(databaseArray[0].name)
+    # # print(databaseArray[0].quantity)
+    #
+    # return HttpResponse(databaseArray[0].name + " " + str(databaseArray[0].quantity))
